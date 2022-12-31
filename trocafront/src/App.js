c0e680bc4f64b93ae68bc8d4eb3805dc252a154e
Martin Nava <martin@mnavapena.com>
2022-12-31 15:17:30 -0600

    UPDATE: prepare menu and routes

--- trocafront/src/App.js
+++ trocafront/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Menu from './components/Menu';
+import NotFound from './components/NotFound';
+import Main from './components/Main';
+import Wallet from './components/Wallet';
+import { PATHS } from './config'
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu />
+      <BrowserRouter>
+        <Routes>
+          <Route path={PATHS.main} element={<Main />}/>
+          <Route path={PATHS.wallet} element={<Wallet />}/>
+          <Route  path='*' element={<NotFound />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
