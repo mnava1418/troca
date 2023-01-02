e3e2b48caf08b04ec23e653c8103cc62318e902a
Martin Nava <martin@mnavapena.com>
2023-01-02 13:40:27 -0600

    ADD: create card link component

---
+++ trocafront/src/components/helpers/CardLink.jsx
@@ -0,0 +1,27 @@
+import Card from 'react-bootstrap/Card'
+
+import '../../styles/Card.css'
+
+function CardLink({title, text, iconStyle}) {
+    return (
+        <Card
+            bg='secondary'
+            key='walletCard'
+            text='white'
+            style={{ width: '18rem', marginTop: '24px' }}
+            className="mb-2 card-link-cust"
+            border="light"
+        >
+            <Card.Body className='d-flex flex-column justify-content-center align-items-center'>
+            <div className='card-img-cust d-flex flex-column justify-content-center align-items-center' style={{marginBottom: '8px'}}>
+                <div className={`bg-img bg-im-contain card-img-cust-icon ${iconStyle}`}/>
+            </div>
+            <Card.Title><h3>{title}</h3></Card.Title>
+            <Card.Text>{text}</Card.Text>
+            </Card.Body>
+        </Card>
+    );
+  }
+  
+  export default CardLink;
+  
