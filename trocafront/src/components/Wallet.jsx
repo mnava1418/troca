24ee89d045bebb49509bdc8c54eb4281ebc870d5
Martin Nava <martin@mnavapena.com>
2023-01-02 13:40:59 -0600

    UPDATE: prepare wallet component

--- trocafront/src/components/Wallet.jsx
+++ trocafront/src/components/Wallet.jsx
@@ -1,8 +1,12 @@
+import CardLink from './helpers/CardLink';
+
 function Wallet() {
     return (
-      <main>
-        Connect your wallet.
-      </main>
+      <section className='d-flex flex-column justify-content-center align-items-center full-screen'>
+        <h1>Connect Your Wallet</h1>
+        <h4>Use MetaMask to start trading your NFTs.</h4>
+        <CardLink title='MetaMask' text='Aqui vamos a poner todo el show' iconStyle='card-img-metamask'/>
+      </section>
     );
   }
   
