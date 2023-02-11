3d7d39f8e62f934c0a644d0b8c2d3d6ba86faec0
mnava1418 <martin@mnavapena.com>
2023-02-10 19:41:34 -0500

    UPDATE: response message

--- trocaback/controllers/ethController.js
+++ trocaback/controllers/ethController.js
@@ -6,7 +6,7 @@ const saveNFTMetaData = async (req, res) => {
     const result = await ethService.saveNFTMetaData(req.originalAccount, title, description, price, royalties, imgData.buffer)
 
     if(result ) {
-        res.status(200).json({message: 'Ok'})
+        res.status(200).json({message: 'Info saved! You will be able to mint after your token is approved. You can check the status on your portfolio.'})
     } else {
         res.status(500).json({error: 'Unable to save metadata.'})
     }
