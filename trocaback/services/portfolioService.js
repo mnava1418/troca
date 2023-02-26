60b9fcf079d0ee4bd7c3e3cc0511273164b7e89b
mnava1418 <martin@mnavapena.com>
2023-02-26 12:48:12 -0500

    UPDATE: get tokens as an array

--- trocaback/services/portfolioService.js
+++ trocaback/services/portfolioService.js
@@ -2,15 +2,20 @@ const admin = require('firebase-admin')
 
 const getAllTokens = async () => {
     const query = admin.database().ref(`/tokens`)
-    let tokens = undefined
+    let tokens = []
 
     await query.once('value', (data) => {
         if(data.exists()) {
-            tokens = data.toJSON()
+            const allTokens = data.toJSON()
+            Object.keys(allTokens).forEach(uri => {
+                const tokenData = {...allTokens[uri], uri}
+                tokens.push(tokenData)
+            })            
         }
     })
     .catch(error => {
         console.error(error)
+        tokens = undefined
     })
 
     return tokens
