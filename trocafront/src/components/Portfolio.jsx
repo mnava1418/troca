91855baa6b1b88f48b7297b35063bc49ab04ea40
mnava1418 <martin@mnavapena.com>
2023-03-20 19:59:23 -0400

    form-ch

--- trocafront/src/components/Portfolio.jsx
+++ trocafront/src/components/Portfolio.jsx
@@ -129,41 +129,42 @@ function Portfolio () {
     const showPage = () => {
         return(
             <section className='full-screen d-flex flex-column justify-content-start align-items-center'>
-                <div style={{backgroundColor: 'var(--bg-color)', width: '100%', height: '24px', position: 'fixed', zIndex: '10'}} />
-                <div className='dark-container form-container form-container-dark search-bar-fixed'>
-                    <Form>
-                        <div className='d-flex justify-content-end align-items-center search-bar'>
-                            <div className='search-bar-check input-group'>
-                                <div className='form-check d-flex flex-row justify-content-center align-items-center'>
-                                    <label className='switch' style={{marginRight: '12px'}}>
-                                        <input type="checkbox" className='form-check-input' defaultChecked={onlyUser} 
-                                            onChange={(e) => {
-                                                dispatch(setOnlyUser(e.target.checked))
-                                                document.getElementById('searchInput').value = ''
-                                                document.getElementById('portfolioSort').value = ''
-                                                search()
-                                            }} 
-                                        />
-                                        <span className='slider round'></span>
-                                    </label>
-                                    <label className="form-check-label">My NFTs</label>
+                <div className='d-flex flex-column justify-content-center align-items-center search-bar-fixed'>
+                    <div className='dark-container form-container form-container-dark' style={{width: '90%'}}>
+                        <Form>
+                            <div className='d-flex align-items-center search-bar'>
+                                <div className='search-bar-check input-group'>
+                                    <div className='d-flex flex-row justify-content-center align-items-center'>
+                                        <label className='switch' style={{marginRight: '12px'}}>
+                                            <input type="checkbox" className='form-check-input' defaultChecked={onlyUser} 
+                                                onChange={(e) => {
+                                                    dispatch(setOnlyUser(e.target.checked))
+                                                    document.getElementById('searchInput').value = ''
+                                                    document.getElementById('portfolioSort').value = ''
+                                                    search()
+                                                }} 
+                                            />
+                                            <span className='slider round'></span>
+                                        </label>
+                                        <label className="form-check-label">My NFTs</label>
+                                    </div>
                                 </div>
+                                <InputGroup className='search-bar-input' style={{marginRight: '0px'}}>
+                                    <Form.Select id='portfolioSort' onChange={sortTokens}>
+                                        <option value=''>Sort by</option>
+                                        <option value='topPrice'>Top Price</option>
+                                        <option value='lowPrice'>Low Price</option>
+                                        <option value='newest'>Newest</option>
+                                        <option value='oldest'>Oldest</option>
+                                    </Form.Select>
+                                </InputGroup>
+                                <InputGroup className='search-bar-input'>
+                                    <Form.Control type="text" id='searchInput' placeholder="address, username or title." onKeyDown={onKeyDown} onKeyUp={search} />
+                                    <InputGroup.Text><i className='bi bi-search' /></InputGroup.Text>
+                                </InputGroup>
                             </div>
-                            <InputGroup style={{marginRight: '0px'}}>
-                                <Form.Select id='portfolioSort' onChange={sortTokens}>
-                                    <option value=''>Sort by</option>
-                                    <option value='topPrice'>Top Price</option>
-                                    <option value='lowPrice'>Low Price</option>
-                                    <option value='newest'>Newest</option>
-                                    <option value='oldest'>Oldest</option>
-                                </Form.Select>
-                            </InputGroup>
-                            <InputGroup className='search-bar-input'>
-                                <Form.Control type="text" id='searchInput' placeholder="address, username or title." onKeyDown={onKeyDown} onKeyUp={search} />
-                                <InputGroup.Text><i className='bi bi-search' /></InputGroup.Text>
-                            </InputGroup>
-                        </div>
-                    </Form>
+                        </Form>
+                    </div>
                 </div>
                 {selectedTokens.length > 0 ? generateCatalog() : emptyCatalog()}
             </section>
