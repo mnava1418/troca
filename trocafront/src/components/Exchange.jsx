ecaa2e83736754e4f076cacb44a6637379029e44
mnava1418 <martin@mnavapena.com>
2023-04-21 22:06:15 -0400

    ADD: exchange template

---
+++ trocafront/src/components/Exchange.jsx
@@ -0,0 +1,45 @@
+import { useDispatch } from 'react-redux'
+import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
+
+import { showExchange } from '../store/slices/exchangeSlice'
+
+import '../styles/Mint.css'
+import '../styles/Exchange.css'
+
+function Exchange() {
+    const dispatch = useDispatch()
+
+    return(
+        <section className='d-flex flex-column justify-content-center align-items-center nft-container' onClick={() => {dispatch(showExchange({show: false}))}}>
+            <div className='exchange-container' onClick={(e) => {e.stopPropagation()}}>
+                <div className='exchange-contents'>
+                    <div className='d-flex flex-column justify-content-center align-items-center'>
+                        <h4>@Buyer</h4>
+                        <div className='d-flex flex-column justify-content-center align-items-center exchange-item'>
+                            <div className='exchange-item-bg bg-img bg-im-contain' />
+                        </div>
+                    </div>
+                    <div className='d-flex flex-column justify-content-center align-items-center exchange-info'>
+                        <Alert key='exchangeAlert' variant='success'>Exchange!</Alert>
+                        <h4>3 ETH</h4>
+                        <div className='exchange-item-bg bg-img bg-im-contain' />
+                    </div>
+                    <div className='d-flex flex-column justify-content-center align-items-center'>
+                        <h4>@Seller</h4>
+                        <div className='d-flex flex-column justify-content-center align-items-center exchange-item'>
+                            <div className='exchange-item-bg bg-img bg-im-contain' />
+                        </div>
+                    </div>
+                </div>
+                <div className='d-flex flex-row justify-content-center align-items-center' style={{marginTop: '40px'}}>
+                    <Button variant="outline-secondary" style={{marginRight: '40px'}}>Reject</Button>
+                    <Button variant="secondary">Accept</Button>
+                    <Button variant="outline-secondary" style={{marginLeft: '40px'}}>Update</Button>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Exchange
