3a4c459c0b1ea8db64476510d6964fd27344c73b
mnava1418 <martin@mnavapena.com>
2023-04-23 17:39:53 -0400

    ADD: update current bid order

--- trocafront/src/store/slices/exchangeSlice.js
+++ trocafront/src/store/slices/exchangeSlice.js
@@ -1,7 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const INITIAL_STATE = {
-    show: false
+    show: false,
+    order: {
+        id: 0,
+        seller: '',
+        sellerData: undefined,
+        buyer: '',
+        buyerData: undefined,
+        price: 0.0,
+        status: '',
+    }
 }
 
 export const exchangeSlice = createSlice({
@@ -10,6 +19,12 @@ export const exchangeSlice = createSlice({
     reducers: {
         showExchange: (state, action) => {
             state.show = action.payload.show
+
+            if(action.payload.show && action.payload.order !== undefined) {
+                state.order = action.payload.order
+            } else {
+                state.order = {...INITIAL_STATE.order}
+            }
         }
     }
 })
