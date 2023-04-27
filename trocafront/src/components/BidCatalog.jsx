9bb8121640315092a24e3af3556a4583ad276488
mnava1418 <martin@mnavapena.com>
2023-04-26 20:47:22 -0400

    UPDATE: refactor code

--- trocafront/src/components/BidCatalog.jsx
+++ trocafront/src/components/BidCatalog.jsx
@@ -1,49 +1,25 @@
-import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
-import { catalogSelector, updateOrderToken } from '../store/slices/exchangeSlice'
-import useWeb3 from '../hooks/useWeb3'
-import Exchange from '../models/Exchange'
+import { updateOrderToken } from '../store/slices/exchangeSlice'
+import { allTokensSelector } from '../store/slices/portfolioSlice'
 
 function BidCatalog({owner, setShowCatalog}) {
     const dispatch = useDispatch()
-    const catalog = useSelector(catalogSelector)
-    const { nft, troca } = useWeb3()
+    const allTokens = useSelector(allTokensSelector)
+    const catalog = Object.values(allTokens).filter(token => token.owner === owner)
 
-    const exchange = new Exchange(dispatch, troca, nft)
-
-    useEffect(() => {
-        if(catalog[owner] === undefined) {
-            exchange.getTokensByAccount(owner)
-        }
-        
-        // eslint-disable-next-line
-    }, [])
-
-    const selectToken = async(e, token) => {
+    const selectToken = async(e, id) => {
         e.stopPropagation()
-
-        const {id, image} = token
-        const imageFile = await fetch(image)
-        const imageData = await imageFile.blob()
-
-        const reader = new FileReader()
-
-        reader.onloadend = () => {
-            const data = reader.result
-            dispatch(updateOrderToken({id, data}))
-            setShowCatalog(false)
-        }
-
-        reader.readAsDataURL(imageData)
+        dispatch(updateOrderToken({id}))
+        setShowCatalog(false)
     }
 
     const generateCatalog = () => {
         return (
             <div className='d-flex flex-row justify-content-center align-items-center flex-wrap' style={{overflow: 'auto', width: '100%', height: '100%'}}>
-                {catalog[owner].map(token => {
+                {catalog.map(token => {
                     return(
-                        <div key={token.id} className='catalog-item bg-img bg-im-cover' style={{backgroundImage: `url("${token.image}")`}} onClick={(e) =>{selectToken(e, token)}}/>
+                        <div key={token.id} className='catalog-item bg-img bg-im-cover' style={{backgroundImage: `url("${token.imageData}")`}} onClick={(e) =>{selectToken(e, token.id)}}/>
                     )
                 })}
             </div>
@@ -52,7 +28,7 @@ function BidCatalog({owner, setShowCatalog}) {
 
     return(
         <>
-            {catalog[owner] !== undefined ? generateCatalog() : <></>}
+            {catalog.length > 0 ? generateCatalog() : <></>}
         </>
     )
 }
