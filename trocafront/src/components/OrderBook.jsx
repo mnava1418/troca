f882de4a63a34b939ca75326ac436f43458ce8cc
mnava1418 <martin@mnavapena.com>
2023-05-08 22:13:32 -0400

    ADD: order book pagination

--- trocafront/src/components/OrderBook.jsx
+++ trocafront/src/components/OrderBook.jsx
@@ -1,6 +1,7 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Table from 'react-bootstrap/Table'
+import Pagination from 'react-bootstrap/Pagination'
 
 import { PATHS } from '../config'
 import { connectionStatusSelector } from '../store/slices/statusSlice'
@@ -10,6 +11,7 @@ import { parseAccount } from '../services/ethServices'
 function OrderBook () {
     const { isConnected } = useSelector(connectionStatusSelector)
     const orderBook = useSelector(orderBookSelector)
+    const [selectedPage, setSelectedPage] = useState(0)
 
     useEffect(() => {
         if(!isConnected) {
@@ -20,9 +22,27 @@ function OrderBook () {
 
     const showEmptyOrderBook = () => {
         return (
-            <div className='d-flex flex-column justify-content-center align-items-center fixed-container' style={{width: '90%'}}>
-                <h4 style={{margin: '100px 0px 24px 0px'}}>We weren't able to find orders.</h4>
+            <div className='d-flex flex-column justify-content-center align-items-center' style={{width: '90%'}}>
+                <h4 style={{margin: '0px 0px 24px 0px'}}>We weren't able to find orders.</h4>
                 <i className="bi bi-emoji-frown" style={{fontSize: '100px', color: 'var(--secondary-color)'}}></i>        
+            </div>
+        )
+    }
+
+    const getPagination = () => {
+        const pages = Math.ceil(orderBook.length / 10 )
+
+        return(
+            <div className='d-flex flex-row justify-content-center align-items-center' style={{width: '100%'}}>
+                <Pagination>
+                    <Pagination.First onClick={() => {setSelectedPage(0)}}/>
+                    <Pagination.Prev onClick={() => {selectedPage > 0 ? setSelectedPage(selectedPage - 1) : <></>}}/>
+                    {[...Array(pages)].map((x, i) => {
+                        return(<Pagination.Item key={`${x}${i}`} active={i === selectedPage} onClick={() => {setSelectedPage(i)}}>{i + 1}</Pagination.Item>)
+                    })}
+                    <Pagination.Next onClick={() => {selectedPage < pages - 1 ? setSelectedPage(selectedPage + 1) : <></>}}/>
+                    <Pagination.Last onClick={() => {setSelectedPage(pages - 1)}}/>
+                </Pagination>
             </div>
         )
     }
@@ -43,7 +63,7 @@ function OrderBook () {
                     </tr>
                     </thead>
                     <tbody>
-                        {orderBook.map((order, index) => {
+                        {orderBook.slice(selectedPage * 10, selectedPage * 10 + 10).map((order, index) => {
                             return(
                                 <tr key={index}>
                                     <td>{order.id}</td>
@@ -58,13 +78,14 @@ function OrderBook () {
                         })}
                     </tbody>
                 </Table>
+                {getPagination()}
             </div>
         )
        
     }
 
     return(
-        <section className='full-screen d-flex flex-column justify-content-start align-items-center'>
+        <section className='full-screen d-flex flex-column justify-content-center align-items-center'>
             {orderBook.length > 0 ? showOrderBook() : showEmptyOrderBook()}
         </section>
     )
