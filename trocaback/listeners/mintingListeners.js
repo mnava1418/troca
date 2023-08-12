bb344b1cd22646dad7ce4ee31ec48fe1804972fb
mnava1418 <martin@mnavapena.com>
2023-08-11 23:21:53 -0400

    DELETE: remove unused method

--- trocaback/listeners/mintingListeners.js
+++ trocaback/listeners/mintingListeners.js
@@ -1,14 +1,6 @@
-const portfolioService = require('../services/portfolioService')
-const socketService = require('../services/socketService')
 const mintService = require('../services/mintService')
-const userService = require('../services/userService')
 
 module.exports = (io, socket) => {
-    socket.on('tokens-available', async () => {
-        const availableTokens = await portfolioService.getAvailableTokens()
-        socket.emit('update-tokens-available', {totalCount: availableTokens.totalCount, availableTokens: availableTokens.available.length})            
-    })
-
     socket.on('generate-token', async(tokenInfo) => {
         console.info(`${socket.account} is generating a new token`)
         const newToken = await mintService.mintToken(socket.account, tokenInfo)
