a1896e89a7a46f6090fd2c1ec683f97e3fd58a8e
mnava1418 <martin@mnavapena.com>
2023-12-23 11:50:18 -0500

    ADD: steps section

---
+++ trocafront/src/components/landing/Steps.jsx
@@ -0,0 +1,53 @@
+function Steps () {
+    return(
+        <section>
+            <div className="container">
+            <div className="cs-seciton_heading cs-style1 text-center wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.2s">
+                <h3 className="cs-section_title cs-font_16 cs-font_14_sm cs-gradient_color">Simple Steps</h3>
+                <h2 className="cs-section_subtitle cs-m0 cs-font_36 cs-font_24_sm">Start Trading</h2>
+            </div>
+            <div className="cs-height_50 cs-height_lg_30"></div>
+            <div className="row wow fadeIn" data-wow-duration="1s" data-wow-delay="0.4s">
+                <div className="col-xl-3 col-md-6">
+                <div className="cs-card cs-style3 text-center cs-light_bg">
+                    <div className="cs-card_in">
+                    <div className="cs-card_number cs-font_30 cs-font_24_sm cs-primary_font cs-body_line_height cs-center">1</div>
+                    <h3 className="cs-card_title cs-font_30 cs-font_24_sm cs-m0">Connect Your Wallet</h3>
+                    </div>
+                </div>
+                <div className="cs-height_25 cs-height_lg_25"></div>
+                </div>
+                <div className="col-xl-3 col-md-6">
+                <div className="cs-card cs-style3 text-center cs-light_bg">
+                    <div className="cs-card_in">
+                    <div className="cs-card_number cs-font_30 cs-font_24_sm cs-primary_font cs-body_line_height cs-center">2</div>
+                    <h3 className="cs-card_title cs-font_30 cs-font_24_sm cs-m0">Become A Member</h3>
+                    </div>
+                </div>
+                <div className="cs-height_25 cs-height_lg_25"></div>
+                </div>
+                <div className="col-xl-3 col-md-6">
+                <div className="cs-card cs-style3 text-center cs-light_bg">
+                    <div className="cs-card_in">
+                    <div className="cs-card_number cs-font_30 cs-font_24_sm cs-primary_font cs-body_line_height cs-center">3</div>
+                    <h3 className="cs-card_title cs-font_30 cs-font_24_sm cs-m0">Mint Using Our AI</h3>
+                    </div>
+                </div>
+                <div className="cs-height_25 cs-height_lg_25"></div>
+                </div>
+                <div className="col-xl-3 col-md-6">
+                <div className="cs-card cs-style3 text-center cs-light_bg">
+                    <div className="cs-card_in">
+                    <div className="cs-card_number cs-font_30 cs-font_24_sm cs-primary_font cs-body_line_height cs-center">4</div>
+                    <h3 className="cs-card_title cs-font_30 cs-font_24_sm cs-m0">Trade Your NFTs</h3>
+                    </div>
+                </div>
+                <div className="cs-height_25 cs-height_lg_25"></div>
+                </div>
+            </div>
+            </div>
+        </section>
+    )
+}
+
+export default Steps
