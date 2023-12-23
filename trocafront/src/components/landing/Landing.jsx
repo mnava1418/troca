1066e798f0762fdf7252090b51ab2768ea335b1c
mnava1418 <martin@mnavapena.com>
2023-12-23 15:56:27 -0500

    UPDATE: generate landing page

--- trocafront/src/components/landing/Landing.jsx
+++ trocafront/src/components/landing/Landing.jsx
@@ -1,12 +1,34 @@
-import Main from "./Main";
+import Main from './Main';
+import Steps from './Steps';
+import Networks from './Networks';
+import FAQ from './FAQ'
+import Contact from './Contact';
+import Footer from './Footer';
+
+//CSS
+import '../../assets/css/plugins/bootstrap.min.css'
+import '../../assets/css/plugins/slick.css'
+import '../../assets/css/plugins/animate.css'
+import '../../assets/css/style.css'
+import '../../styles/Main.css'
 
 function Landing() {
     return (
         <>
             <Main />
+            <div className="cs-height_70 cs-height_lg_40"></div>
+            <Steps />
+            <div className="cs-height_70 cs-height_lg_40"></div>
+            <Networks />
+            <div className="cs-height_70 cs-height_lg_40"></div>
+            <FAQ />
+            <div className="cs-height_70 cs-height_lg_40"></div>
+            <Contact />
+            <div className="cs-height_70 cs-height_lg_40"></div>
+            <Footer />
         </>
-    );
-  }
-  
-  export default Landing;
+    )
+}
+        
+export default Landing;
   
