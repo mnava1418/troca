9ef45156d24f4325faa85515690128cb7c32fb91
mnava1418 <martin@mnavapena.com>
2023-12-23 16:04:16 -0500

    UPDATE: wording

--- trocafront/src/components/landing/Steps.jsx
+++ trocafront/src/components/landing/Steps.jsx
@@ -4,7 +4,7 @@ function Steps () {
             <div className="container">
             <div className="cs-seciton_heading cs-style1 text-center wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.2s">
                 <h3 className="cs-section_title cs-font_16 cs-font_14_sm cs-gradient_color">Simple Steps</h3>
-                <h2 className="cs-section_subtitle cs-m0 cs-font_36 cs-font_24_sm">Start Trading</h2>
+                <h2 className="cs-section_subtitle cs-m0 cs-font_36 cs-font_24_sm">How do I start?</h2>
             </div>
             <div className="cs-height_50 cs-height_lg_30"></div>
             <div className="row wow fadeIn" data-wow-duration="1s" data-wow-delay="0.4s">
@@ -30,7 +30,7 @@ function Steps () {
                 <div className="cs-card cs-style3 text-center cs-light_bg">
                     <div className="cs-card_in">
                     <div className="cs-card_number cs-font_30 cs-font_24_sm cs-primary_font cs-body_line_height cs-center">3</div>
-                    <h3 className="cs-card_title cs-font_30 cs-font_24_sm cs-m0">Use AI To Mint</h3>
+                    <h3 className="cs-card_title cs-font_30 cs-font_24_sm cs-m0">Use AI To Mint Your NFTs</h3>
                     </div>
                 </div>
                 <div className="cs-height_25 cs-height_lg_25"></div>
@@ -39,7 +39,7 @@ function Steps () {
                 <div className="cs-card cs-style3 text-center cs-light_bg">
                     <div className="cs-card_in">
                     <div className="cs-card_number cs-font_30 cs-font_24_sm cs-primary_font cs-body_line_height cs-center">4</div>
-                    <h3 className="cs-card_title cs-font_30 cs-font_24_sm cs-m0">Trade Your NFTs</h3>
+                    <h3 className="cs-card_title cs-font_30 cs-font_24_sm cs-m0">Start Trading Your NFTs</h3>
                     </div>
                 </div>
                 <div className="cs-height_25 cs-height_lg_25"></div>
