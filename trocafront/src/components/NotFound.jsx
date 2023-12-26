9887a6865fa274c472a9164c9755e85e0ce97e58
mnava1418 <martin@mnavapena.com>
2023-12-25 20:53:05 -0500

    UPDATE: styles

--- trocafront/src/components/NotFound.jsx
+++ trocafront/src/components/NotFound.jsx
@@ -4,7 +4,7 @@ import { PATHS } from '../config';
 
 function NotFound() {
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center" style={{textAlign: 'center'}}>
+    <div className="d-flex flex-column justify-content-center align-items-center full-screen" style={{textAlign: 'center'}}>
       <h1 style={{fontSize: '72px'}}>404 Not Found</h1>
       <p style={{fontSize: '24px', textAlign: 'center', marginBottom: '40px'}}>Sorry, the page you are looking for is not available.</p>
       <Button variant="primary" onClick={() => {window.location.href = PATHS.main}}>Go Home</Button>
